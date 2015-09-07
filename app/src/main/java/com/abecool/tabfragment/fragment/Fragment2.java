df966be851ea17bb2d20cf82c6c4c965b7c0088a
linyt <linyituo@abecool.com>
2015-09-07 18:38:08 +0800

    init project

---
+++ app/src/main/java/com/abecool/tabfragment/fragment/Fragment2.java
@@ -0,0 +1,36 @@
+package com.abecool.tabfragment.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTabHost;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.abecool.tabfragment.R;
+
+public class Fragment2 extends BaseFragment {
+
+	public static BaseFragment newInstance(int index) {
+		BaseFragment fragment = new Fragment2();
+		Bundle args = new Bundle();
+		args.putInt("index", index);
+		fragment.setArguments(args);
+		fragment.setIndex(index);
+		return fragment;
+	}
+
+	private View layoutView;
+	private FragmentTabHost mTabHost;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+
+		layoutView = inflater.inflate(R.layout.fragment2, null);
+		return layoutView;
+	}
+}
